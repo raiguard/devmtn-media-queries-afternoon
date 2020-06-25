fab916b0eb978c787b449771dbc0dc460d8cec09
raiguard <caleb.heuer@gmail.com>
2020-06-24 18:01:27 -0600

    Get plain old navbar working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./navbar-logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <div className="navbar-container">
+          <img src={logo} className="navbar-logo" alt="Start Bootstrap" />
+          <nav>
+            <span className="nav-button">SERVICES</span>
+            <span className="nav-button">PORTFOLIO</span>
+            <span className="nav-button">ABOUT</span>
+            <span className="nav-button">TEAM</span>
+            <span className="nav-button">CONTACT</span>
+          </nav>
+          <div className="mobile-nav-hamburger">MENU</div>
+        </div>
       </header>
+      <div className="welcome-section"></div>
     </div>
   );
 }
